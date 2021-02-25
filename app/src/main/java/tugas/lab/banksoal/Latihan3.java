841d81b0ffa93bbf47ee075f7911e8ea6a5ec553
AveroStone <muhammadirsyad.ti.2019302029@gmailc.om>
2021-02-25 07:26:24 +0700

    Averroes

---
+++ app/src/main/java/tugas/lab/banksoal/Latihan3.java
@@ -0,0 +1,14 @@
+package tugas.lab.banksoal;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Latihan3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_latihan3);
+    }
+}
